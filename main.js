eafef84978326d68a73977587b027c18e2e908d3
dixxyy <saputradicky482@gmail.com>
2023-12-28 16:42:02 +0700

    belajar create element html by javascript

--- main.js
+++ main.js
@@ -1,45 +1,16 @@
-// tugas kecil 1
+document.title = 'dixxyycuy';
 
-/*
-1. prompt untuk mengetahui saldo akhir dari apa yang diinputkan user
-2. menentukan hari dan tanggal yang ada saat ini di pc
-3. jangan gunakan chatgpt ya broo
-**/
+const body = document.body;
 
-let saldoAwal = 100000;
-let saldoGaji = 300000;
+const h1 = document.createElement('h1');
+h1.textContent = 'ini h1 cuyy';
 
-const saldoAkhir = saldoAwal + saldoGaji;
+const nama = document.createElement('h2');
+nama.innerHTML = '<marquee>dixxyy nih cuyy</marquee>';
 
-alert(`saya memiliki uang sebesar Rp.${saldoAwal} lalu baru gajian sebesar Rp.${saldoGaji} sehingga uang saya sekarang adalah ${saldoAkhir}`);
+const cuy = document.createElement('p');
+cuy.innerHTML = '<marquee>sayang</marquee>';
 
-const hari = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu'];
-
-let hariIni = new Date().getDay();
-
-switch (hariIni) {
-  case 1:
-    hari = hari[0];
-    break;
-  case 2:
-    hari = hari[1];
-    break;
-  case 3:
-    hari = hari[2];
-    break;
-  case 4:
-    alert(`hari ini adalah hari ${hari[3]}`);
-    console.log(hari[3]);
-    break;
-  case 5:
-    hari = hari[4];
-    break;
-  case 6:
-    hari = hari[5];
-    break;
-  case 7:
-    hari = hari[6];
-    break;
-  default:
-    hari = hari[7];
-}
+body.append(h1);
+body.append(nama);
+body.append(cuy);
