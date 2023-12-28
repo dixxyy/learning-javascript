958714387d17f4e89554da30c90bbc1b7dcab87a
dixxyy <saputradicky482@gmail.com>
2023-12-28 17:04:35 +0700

    get element by id and querySelector

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 document.title = 'dixxyycuy';
 
-const body = document.body;
+const btn1 = document.getElementById('btn-1');
+const btn2 = document.querySelector('.btn-2');
 
-const h1 = document.createElement('h1');
-h1.textContent = 'ini h1 cuyy';
+btn1.style.border = 'none';
+btn1.style.background = 'grey';
+btn1.style.fontSize = '24px';
+btn1.style.padding = '5px';
 
-const nama = document.createElement('h2');
-nama.innerHTML = '<marquee>dixxyy nih cuyy</marquee>';
-
-const cuy = document.createElement('p');
-cuy.innerHTML = '<marquee>sayang</marquee>';
-
-body.append(h1);
-body.append(nama);
-body.append(cuy);
+console.log(btn2);
