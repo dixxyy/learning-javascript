239811c1df8aa465b546171a8141ed9cb15bd3af
dixxyy <saputradicky482@gmail.com>
2023-12-28 17:37:27 +0700

    event combination with onmouse over and out

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ document.title = 'dixxyycuy';
 const btn1 = document.getElementById('btn-1');
 const btn2 = document.querySelector('.btn-2');
 
+const defaultText = 'klik cuy 1';
+
+btn1.textContent = defaultText;
+
 btn1.style.border = 'none';
 btn1.style.background = 'grey';
 btn1.style.fontSize = '24px';
@@ -11,6 +15,13 @@ btn1.style.padding = '5px';
 console.log(btn2);
 
 function gantiWarna() {
-  console.log('aman');
   btn1.style.background = 'lightblue';
+}
+
+function hoverIn() {
+  btn1.textContent = 'kepencet';
+}
+
+function hoverOut() {
+  btn1.textContent = defaultText;
 }
