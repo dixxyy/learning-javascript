f5be8844f19f5ebec4d9e1a3a1359b39194c8130
dixxyy <saputradicky482@gmail.com>
2023-12-29 18:15:38 +0700

    tugas kecil 2 ubah warna mouse over & out

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.title = 'dixxyycuy';
+const body = document.body;
 
 const btn1 = document.getElementById('btn-1');
 const btn2 = document.querySelector('.btn-2');
@@ -7,6 +8,9 @@ const defaultText = 'klik cuy 1';
 
 btn1.textContent = defaultText;
 
+const newText = document.createElement('h1');
+newText.textContent = 'aku dixxyy';
+
 btn1.style.border = 'none';
 btn1.style.background = 'grey';
 btn1.style.fontSize = '24px';
@@ -14,14 +18,20 @@ btn1.style.padding = '5px';
 
 console.log(btn2);
 
-function gantiWarna() {
+function clickButton() {
   btn1.style.background = 'lightblue';
+  const newText = document.createElement('p');
+  newText.textContent = 'aku dixxyy';
+  body.append(newText);
 }
 
 function hoverIn() {
   btn1.textContent = 'kepencet';
+  newText.style.color = 'blue';
+  body.append(newText);
 }
 
 function hoverOut() {
-  btn1.textContent = defaultText;
+  newText.style.color = 'tomato';
+  body.append(newText);
 }
