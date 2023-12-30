68a6ef4617508fda398801a3ac43019349e0f1e2
dixxyy <saputradicky481@gmail.com>
2023-12-31 02:50:42 +0700

    while looping

--- main.js
+++ main.js
@@ -5,3 +5,12 @@
 for (let i = 1; i <= 5; i++) {
   if (i % 2 == 1) console.log('data ganjil adalah ', i);
 }
+for (let i = 1; i <= 5; i++) {
+  if (i % 2 == 0) console.log('data genap adalah ', i);
+}
+
+let j = 1;
+while (j <= 10) {
+  console.log('data j ke - ', j);
+  j++;
+}
