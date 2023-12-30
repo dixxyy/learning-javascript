c34a3f9b775b97590292b74ce30b4689d9c21c72
dixxyy <saputradicky482@gmail.com>
2023-12-31 01:07:06 +0700

    rumus lingkaran

--- main.js
+++ main.js
@@ -1,37 +1,25 @@
-document.title = 'dixxyycuy';
-const body = document.body;
+/*
+  TUGAS BESAR: 
+  1 MEMBUAT PERHITUNGAN MATEMATIKA
+  - luas lingkaran
+  - luas segitiga
+  - luas persegi panjang
+  - luas jajar genjang
+  
+  2. menghitung total gaji yang didapat dalam satu bulan
+    dengan input minimal:
+    - nama karyawan
+    - gaji perhari
+    - jumlah hari masuk kerja
+*/
 
-const btn1 = document.getElementById('btn-1');
-const btn2 = document.querySelector('.btn-2');
+// let x = prompt('masukkan angka pertama: '); // Prompt akan menampilkan dialog untuk pengguna memasukkan nilai
+// let y = prompt('masukkan angka kedua: '); // Prompt akan menampilkan dialog untuk pengguna memasukkan nilai
 
-const defaultText = 'klik cuy 1';
+const phi = 3.14;
 
-btn1.textContent = defaultText;
+const lingkaran = (r) => {
+  console.log('luas lingkaran adalah: ', phi * r * r);
+};
 
-const newText = document.createElement('h1');
-newText.textContent = 'aku dixxyy';
-
-btn1.style.border = 'none';
-btn1.style.background = 'grey';
-btn1.style.fontSize = '24px';
-btn1.style.padding = '5px';
-
-console.log(btn2);
-
-function clickButton() {
-  btn1.style.background = 'lightblue';
-  const newText = document.createElement('p');
-  newText.textContent = 'aku dixxyy';
-  body.append(newText);
-}
-
-function hoverIn() {
-  btn1.textContent = 'kepencet';
-  newText.style.color = 'blue';
-  body.append(newText);
-}
-
-function hoverOut() {
-  newText.style.color = 'tomato';
-  body.append(newText);
-}
+lingkaran(5);
