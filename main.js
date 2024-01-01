b15f7719a43cb57bf2e04edb255ec6bed759e001
dixxyy <saputradicky481@gmail.com>
2024-01-02 01:30:27 +0700

    async login with token and apikey

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const token = ~~[Math.random() * 12345678];
 
+const pictures = ['1.jpg', '2.jpg', '3.jpg'];
+
 function login(username) {
   return { token, username };
 }
 
+function getUser(token) {
+  if (token) return { apiKey: 'xkey123' };
+}
+
+function getPictures(apiKey) {
+  if (apiKey) return pictures;
+}
+
 const user = login('dixxyy');
 
-console.log(user);
+console.log(user.token);
+console.log(user.username);
+const { apiKey } = getUser(user.token);
+console.log(apiKey);
+
+const getUserPicture = getPictures(apiKey);
+
+console.log(getUserPicture);
