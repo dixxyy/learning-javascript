80de7e093ec3d666ba6dd59899f7f65d567b55a8
dixxyy <saputradicky481@gmail.com>
2024-01-05 11:16:55 +0700

    making browser localstorage

--- main.js
+++ main.js
@@ -1,51 +1,50 @@
-const token = ~~[Math.random() * 12345678];
+let usernameInput = document.getElementById('usernameInput');
+let passwordInput = document.getElementById('passwordInput');
+let loginButton = document.getElementById('loginButton');
+let logoutButton = document.getElementById('logoutButton');
+let admin = document.getElementById('admin');
+let basic = document.getElementById('basic');
 
-const pictures = ['1.jpg', '2.jpg', '3.jpg'];
+admin.style.display = 'none';
+basic.style.display = 'none';
+logoutButton.style.display = 'none';
 
-function login(username) {
-  console.log('prosessing data..');
-  return new Promise((success, failed) => {
-    setTimeout(() => {
-      if (username != 'dixxyy') failed('nyari apa hayo');
-      success({ token });
-    }, 500);
-  });
-}
-
-function getUser(token) {
-  console.log('prosessing token..');
-  return new Promise((success, failed) => {
-    if (!token) failed('sorry no token, cannot access');
-    setTimeout(() => {
-      success({ apiKey: 'xkey123' });
-    }, 500);
-  });
-}
-
-function getPictures(apiKey) {
-  console.log('prosessing picture..');
-  return new Promise((success, failed) => {
-    if (!apiKey) failed('mana apiKey nya bjir');
-    setTimeout(() => {
-      success({ pic: pictures });
-    }, 1500);
-  });
-}
-
-async function UserDisplay() {
-  try {
-    const { token } = await login('dixxyy');
-    const { apiKey } = await getUser(token);
-    const { pic } = await getPictures(apiKey);
-
-    console.log(`
-    token anda adalah: ${token}
-    apiKey anda adalah: ${apiKey}
-    hasil request gambar sebagai berikut: ${pic}
-    `);
-  } catch (err) {
-    console.log(err);
+function onLogin() {
+  localStorage.setItem('username', usernameInput.value);
+  logoutButton.style.display = 'block';
+  if (usernameInput.value == 'admin' && passwordInput.value == 'admin123') {
+    localStorage.setItem('role', 'admin');
+    usernameInput.style.display = 'none';
+    passwordInput.style.display = 'none';
+    loginButton.style.display = 'none';
+    admin.style.display = 'block';
+    basic.style.display = 'none';
+  } else {
+    localStorage.setItem('role', 'basic');
+    usernameInput.style.display = 'none';
+    passwordInput.style.display = 'none';
+    loginButton.style.display = 'none';
+    admin.style.display = 'none';
+    basic.style.display = 'block';
   }
 }
 
-UserDisplay();
+if (localStorage.getItem('username')) {
+  usernameInput.style.display = 'none';
+  passwordInput.style.display = 'none';
+  loginButton.style.display = 'none';
+  logoutButton.style.display = 'block';
+  if (localStorage.getItem('role' == 'admin')) {
+    admin.style.display = 'block';
+    basic.style.display = 'none';
+    logoutButton.style.display = 'block';
+  } else {
+    admin.style.display = 'none';
+    basic.style.display = 'block';
+  }
+}
+
+function onLogout() {
+  localStorage.clear();
+  location.reload();
+}
